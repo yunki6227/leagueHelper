ba4e27b873b44bbffae26e977a66467d0e5d66c6
yunki6227 <yunki6227@gmail.com>
2022-12-13 17:49:55 -0500

    added gitignore for venv and removed venv from github

--- champInterpreter/imageCapture.py
+++ champInterpreter/imageCapture.py
@@ -7,3 +7,11 @@ from PIL import ImageChops
 # return some data with image information and player information, perhaps in a tuple (player,image)
 # Example: (top1, image_top1) would mean top player from team 1, and its corresponding image
 # could continue like top1,top2,jg1,jg2,mid1,mid2,adc1,adc2,sup1,sup2
+
+
+# calls captureImage 10 times to capture 
+def captureNames():
+    return 0
+
+def captureImage(x,y,width,height):
+    return 0
