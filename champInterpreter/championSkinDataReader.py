900d65d5d60a431e23ac5a96dceb26bb8c19ec74
yunki6227 <yunki6227@gmail.com>
2022-12-19 16:15:21 -0500

    finished championSkinDataReader.py's method, getSkinInfos(), that returns a tuple of skinname and championname related to it

---
+++ champInterpreter/championSkinDataReader.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+# returns a list of tuple (skin_name,champion_name)
+def getSkinInfos():
+    try:
+        arraySkinChamp=[]
+        link='https://leagueoflegends.fandom.com/wiki/List_of_champion_skins_(League_of_Legends)'
+        source = requests.get(link,headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}).text
+        soup = BeautifulSoup(source,"html.parser")
+        allSkinInfos = soup.find_all("td",{"class": "skin-icon"})
+        for skin in allSkinInfos:
+            arraySkinChamp.append((skin.getText(),skin.get("data-champion")))
+        return arraySkinChamp
+    except:
+         print("Error in getting skins information")
+         return [("","")]
+
+#getSkinInfos()
