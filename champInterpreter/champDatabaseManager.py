245f11cc71741def1457022aca0df5c2b8af7096
yunki6227 <yunki6227@gmail.com>
2022-12-19 01:21:07 -0500

    added method to connect, reset, and create tables that holds on to league of legends champion skins information. If table doesn't already exist when connecting, then create table

--- champInterpreter/champDatabaseManager.py
+++ champInterpreter/champDatabaseManager.py
@@ -1,25 +1,28 @@
 import sqlite3
 
-connection = None
 
 def connect():
-    if connection is None:
-        connection=sqlite3.connect("leagueChampSkins.sqlite3")
-    createTables()
+    connection=sqlite3.connect(r"resource\leagueChampSkins.sqlite3")
+    cursor = connection.cursor()
+    try:
+        query = "SELECT * FROM skins"
+        cursor.execute(query)
+    except sqlite3.OperationalError:
+        createTables(connection)
+    return connection
 
-def reset():
-    if connection is None:
-        return 0
+def reset(connection):
     cursor = connection.cursor()
     query = "DROP skins"
     cursor.execute(query)
     connection = None
 
-def createTables():
+def createTables(connection):
     cursor = connection.cursor()
     query = "CREATE TABLE IF NOT EXISTS skins (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, skin_name VARCHAR(255) NOT NULL, champion_name VARCHAR(255) NOT NULL);"
     cursor.execute(query)
 
-def populateData():
+#def populateData():
     # uses method from championSkinDataReader to fill data up in sqlite3
     
+connect()
