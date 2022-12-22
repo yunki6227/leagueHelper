8198d696d79e580e5b3d9507241e82a71b4710bb
Yunki <kmj9ab@virginia.edu>
2022-12-22 05:57:02 -0500

    used selenium to calculate approximate team winrate depending on the opposing laner. Need to optimize: it's slow

--- apiReader/opggReader.py
+++ apiReader/opggReader.py
@@ -4,13 +4,12 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 def getTeamWinrate(names):
-    try:
-        totalWinrate=0
-        for i in range(5):
-            totalWinrate+=getChampWinrate(names[i],names[i+5],i)
-        return round(totalWinrate/5,2)
-    except:
-        print("Error in obtaining champion(s) winrate(s)")   
+    totalWinrate=0
+    for i in range(5):
+        totalWinrate+=getChampWinrate(names[i],names[i+5],i)
+    print(str(round(totalWinrate/5,2))+"%")
+    return round(totalWinrate/5,2)   
+
 def getChampWinrate(champion1,champion2,laneNum):
     lane=""
     if laneNum == 0:
@@ -28,9 +27,23 @@ def getChampWinrate(champion1,champion2,laneNum):
     driver.get("https://www.op.gg/champions/"+champion1+"/"+lane+"/counters?region=global&tier=all&target_champion="+champion2)
     source = driver.page_source
     soup = BeautifulSoup(source,"html.parser")
-    winrate = soup.find("span",{"class":"percent"}).getText()
-    winrate = float(winrate.replace('%',''))
-    return winrate
+    winrate = soup.find("span",{"class":"percent"})
+    if winrate:
+        winrate=winrate.getText()
+        winrate = float(winrate.replace('%',''))
+        print(winrate)
+        return winrate
+    else:
+        driver.get("https://www.op.gg/champions/"+champion2+"/"+lane+"/counters?region=global&tier=all&target_champion="+champion1)
+        source = driver.page_source
+        soup = BeautifulSoup(source,"html.parser")
+        winrate = soup.find("span",{"class":"percent"}).getText()
+        winrate = 100-float(winrate.replace('%',''))
+        print(winrate)
+        return winrate
+
+testNames=['Garen','Rengar','Viego','Zeri','Lux','Irelia','Hecarim','Akali','Jhin','Anivia']
+getTeamWinrate(testNames)
 
 # def getTeamWinrateOverTime():
 
