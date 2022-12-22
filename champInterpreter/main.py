a312a81e0c37ab33e360332822933fc266104968
Yunki <kmj9ab@virginia.edu>
2022-12-22 08:06:02 -0500

    changed main.py to correspondingly change order of names if the user's team is on the bottom

--- champInterpreter/main.py
+++ champInterpreter/main.py
@@ -1,9 +1,14 @@
 import findChampion
 import opggReader
-import champDatabaseManager
+import imageCapture
 import time
 def main():
     names = findChampion.getChampionNames()
+    if not imageCapture.isTopMyTeam:
+        for i in range(5):
+            temp=names[i]
+            names[i]=names[i+5]
+            names[i+5]=temp
     opggReader.getTeamWinrate(names)
     return 0
 
