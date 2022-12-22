6bc46b1bd58a608c7c887d92784d34e2fd9fbd80
Yunki <kmj9ab@virginia.edu>
2022-12-21 20:20:25 -0500

    added methods in findChampion to convert list of names, which could be skin name or champion name, all to champion name corresponding to it

--- champInterpreter/findChampion.py
+++ champInterpreter/findChampion.py
@@ -1,3 +1,12 @@
 # gets player info and string from image by calling methods from imageToString.py
 # goes through the list of champions and skins then returns player info and champion name
 # if the skin name does not exist in the database, rerun connection in champDatabaseManager to 
+
+import champDatabaseManager
+
+def convertToChampionName(names):
+    champion_names=[]
+    connection = champDatabaseManager.connect()
+    for name in names:
+        champion_names.append(champDatabaseManager.getChampion(connection,name))
+    return champion_names
