54fd4071bb05e8c46a5fd1aeecdba9d73bcec4ac
yunki6227 <yunki6227@gmail.com>
2022-12-22 01:38:46 -0500

    added some notes and started with methods in opggReader, getTeamWinrate and getChampWinrate

--- apiReader/opggReader.py
+++ apiReader/opggReader.py
@@ -1,18 +1,31 @@
 # contains methods that scrape data from op.gg
+import requests
+from bs4 import BeautifulSoup
+
+def getTeamWinrate(names):
+    try:
+        totalWinrate=0
+        for i in range(5):
+            totalWinrate+=getChampWinrate(names[i],names[i+5],i)
+        return round(totalWinrate/5,2)
+    except:
+        print("Error in obtaining champion(s) winrate(s)")   
+def getChampWinrate(champion1,champion2,laneNum):
+    lane=""
+    if laneNum == 0:
+        lane = "top"
+    elif laneNum == 1:
+        lane = "jungle"
 
 
-def getTeamWinrate():
+# def getTeamWinrateOverTime():
 
-def getChampWinrate(champion1,champion2):
+# def getChampWinrateOverTime():
 
-def getTeamWinrateOverTime():
+# def getTeamItemRecommended():
 
-def getChampWinrateOverTime():
+# def getChampItemRecommended(champion1,champion2):
 
-def getTeamItemRecommended():
+# def getTeamRuneRecommended():
 
-def getChampItemRecommended(champion1,champion2):
-
-def getTeamRuneRecommended():
-
-def getChampRuneRecommended(champion1,champion2):
+# def getChampRuneRecommended(champion1,champion2):
